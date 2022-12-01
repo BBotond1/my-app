091d7abc7863f477c2052e8457c529708d93220a
Errorcode07 <bbotond0829@gmail.com>
2022-12-01 17:40:12 +0200

    kedvenc pizza -csillag

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
-import "./Pizza.css"
+import React, { useEffect, useState } from "react";
+import "./Pizza.css";
 
-function Pizza({name, id, ingredients, price, picture}) { /* destructuring object */
-  
-const [isFavorite, setIsFavorite] = useState(false)
+function Pizza({ name, id, ingredients, price, picture }) {
+  /* destructuring object */
+
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(
+    function () {
+      console.log(isFavorite);
+    },
+    
+  );
+
   return (
-    <div className={'Pizza' + (isFavorite ? ' fav' : '')}>
-      <img onClick = {() =>
-        setIsFavorite(true)
-      }src="https://img.icons8.com/fluency/48/000000/star.png" alt="csillag" 
-      className='favorite'/>
+    <div className={"Pizza" + (isFavorite ? " fav" : "")}>
+      <img
+        onClick={() => setIsFavorite(!isFavorite)}
+        src={
+          isFavorite
+            ? "https://www.iconpacks.net/icons/2/free-favourite-icon-2765-thumb.png"
+            : "https://img.icons8.com/fluency/48/000000/star.png"
+        }
+        alt="csillag"
+        className="favorite"
+      />
       <h3>{id}</h3>
-      <h1>{name} {isFavorite ? 'Kedvenc' : 'Nem kedvenc'}</h1>
+      <h1>{name}</h1>
       <h3>Osszetevok: {ingredients}</h3>
       <h2>{price}</h2>
+      <img src={"pictures/" + picture} alt="pizza_picture"></img>
     </div>
-  )
+  );
 }
 
-export default Pizza
+export default Pizza;
--- src/components/Pizzas.js
+++ src/components/Pizzas.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import {pizzas} from "../data/pizza_data.js"
-import Pizza from './Pizza'
+import React, { useEffect, useState } from "react";
+// import { pizzas } from "../data/pizza_data.js";
+import Pizza from "./Pizza";
 
 function Pizzas() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(function () {
+    fetch(`http://127.0.0.1:4001/`)
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data){
+        return setPizzas(data)
+      })
+  },[])
+
   return (
     <div>
-      {pizzas.map(pizza => <Pizza key={pizza.id} name={pizza.name} id={pizza.id} ingredients={pizza.ingredients}/>)}
+      {pizzas.map((pizza) => (
+        <Pizza
+          key={pizza.id}
+          name={pizza.name}
+          id={pizza.id}
+          ingredients={pizza.ingredients}
+          picture={pizza.picture}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Pizzas
+export default Pizzas;
